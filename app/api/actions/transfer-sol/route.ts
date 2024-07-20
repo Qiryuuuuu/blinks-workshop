248c8eaa84e72b1bd9181369a0cf8fee3f20b04a
Joniel Bolocon <133493937+Qiryuuuuu@users.noreply.github.com>
2024-07-20 14:46:24 +0800

    Update route.ts

--- app/api/actions/transfer-sol/route.ts
+++ app/api/actions/transfer-sol/route.ts
@@ -5,7 +5,7 @@ export const GET = async (req: Request) => {
     const payload: ActionGetResponse = {
         title: "Test your luck! and win amazing prizes",
         icon: "https://w0.peakpx.com/wallpaper/288/839/HD-wallpaper-world-cup-trophy-art-yellow-art-world-cup-trophy.jpg",
-        description: "Get a chance to get amazing rewards by only 0.0001 SOL!",
+        description: "Get a chance to get amazing rewards by only 1 SOL!",
         label: "Register"
     }
 
@@ -18,12 +18,12 @@ export const OPTIONS = GET;
 
 export const POST = async (req: Request) => {
     const body: ActionPostRequest = await req.json();
-    const transaction = await transferSolTransaction({ from: body.account, amount: 0.0001})
+    const transaction = await transferSolTransaction({ from: body.account, amount: 1})
 
     const payload: ActionPostResponse = await createPostResponse({
         fields: {
             transaction,
-            message: `Send 0.0001 SOL`,
+            message: `Send 1 SOL`,
         },
     });
     return Response.json(payload, {
